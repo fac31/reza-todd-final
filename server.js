00a4fcca8e34bd2401b243c1b01b3133779ecfe8
“rezan5910” <bytemefirst@gmail.com>
2024-05-23 19:33:49 +0100

    feat: server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.disable("x-powered-by");
+app.enable("trust proxy");
+
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    // res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+    res.status(200).send("Hello World");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
