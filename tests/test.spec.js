f528a0bff7b537ccc69b91478ecaab1b44d56d6c
“rezan5910” <bytemefirst@gmail.com>
2024-05-24 01:07:08 +0100

    tests fix maybe

--- tests/test.spec.js
+++
@@ -1,7 +0,0 @@
-const {test, expect} = require('@playwright/test');
-test('Home Page test', async ({page}) => {
-    await page.goto('/');
-    await expect(page.getByRole('heading')).toContainText('Hello World!!!');
-
-})
-;
