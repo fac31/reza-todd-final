cf4ff8d8061f1e72d2914fdad20cfcede9a9811b
Todd <toddtaylor189@gmail.com>
2024-05-25 23:53:49 +0100

    initialising rotating javascript

--- public/JS/city.js
+++ public/JS/city.js
@@ -1,5 +1,5 @@
-const str = "Welcome to RADAR";
-const text = document.getElementById("welcomeText");
+const str = "Today, Tomorrow, Day3, Day4, Day5, Day6, Day7";
+const text = document.getElementById("weatherText");
 window.onload = function(){
     for(let i=0; i<str.length; i++) {
         let span = document.createElement('span');
