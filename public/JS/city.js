027c91c6f9015bbff65143dace3f534d48ecb954
Todd <toddtaylor189@gmail.com>
2024-05-31 17:59:57 +0100

    beginning to get the carousel to function

--- public/JS/city.js
+++ public/JS/city.js
@@ -9,3 +9,25 @@ window.onload = function(){
         span.style.transform = 'rotate(${11 * i}deg)';
     }
 }
+
+
+
+// JavaScript for carousel functionality
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slides = document.querySelector('.slides');
+const slideWidth = slides.querySelector('img').clientWidth;
+
+prevButton.addEventListener('click', () => {
+  slides.scrollBy({
+    left: -slideWidth,
+    behavior: 'smooth'
+  });
+});
+
+nextButton.addEventListener('click', () => {
+  slides.scrollBy({
+    left: slideWidth,
+    behavior: 'smooth'
+  });
+});
