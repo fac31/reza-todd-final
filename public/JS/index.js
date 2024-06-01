076f922c4e7af59577e72c2428aec34b7a16768a
Todd <toddtaylor189@gmail.com>
2024-06-01 16:07:45 +0100

    functioning index welcome

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,21 +1,13 @@
-
-// ********* Javascript for dynamic welcome sign *************
-const str = "Welcome to RADAR. Welcome to RADAR. Welcome to RADAR.";
-const circle = document.querySelector('.indexWelcomeCircle');
-
-window.onload = function() {
+// ********** Rotating welcome text ************
+const str = "Welcome to RADAR.";
+const text = document.getElementById("text");
+window.onload = function(){
     for (let i = 0; i < str.length; i++) {
         let span = document.createElement('span');
-        span.innerHTML = str[i];
-        
-        // Adjust the distance from the center to move the text inward
-        let distanceFromCenter = 250; // Change this value as needed
-
-        // Calculate the position for each character
-        let angle = i * (360 / str.length);
-        span.style.transform = `rotate(${angle}deg) translate(0, -${distanceFromCenter}px) rotate(-${angle}deg)`;
-        
-        circle.appendChild(span);
+        span.innerHTML = str[i] 
+        text.appendChild(span);
+        console.log(str[i])
+        span.style.transform = `rotate(${11*i}deg)`;
     }
 }
 
