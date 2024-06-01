301187ce2ed79364df08b4c7fd1904bd240c27a0
Todd <toddtaylor189@gmail.com>
2024-06-01 17:38:36 +0100

    functioning weather circle

--- public/JS/city.js
+++ public/JS/city.js
@@ -1,12 +1,12 @@
-const str = "Today, Tomorrow, Day3, Day4, Day5, Day6, Day7";
+const str = "1/1/1 High-Low Wind:_mph Icon";
 const text = document.getElementById("weatherText");
 window.onload = function(){
-    for(let i=0; i<str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
         let span = document.createElement('span');
-        span.innerHTML = str[i];
+        span.innerHTML = str[i] 
         text.appendChild(span);
         console.log(str[i])
-        span.style.transform = 'rotate(${11 * i}deg)';
+        span.style.transform = `rotate(${11*i}deg)`;
     }
 }
 
